b9fcd3e6940288aff7dfb13274ea68bce7b2e11b
Duong Hoang Long <longmsi147@gmail.com>
2025-07-04 15:33:25 +0700

    test ECR

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet! Add!</p>
+                <p className="text-center">You have no todo items yet! Add! Test ECR</p>
             )}
             {items.map(item => (
                 <ItemDisplay
