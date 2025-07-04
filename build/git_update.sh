8c5f5f823b586fb56acf04fa95fe57cbeba50c26
Duong Hoang Long <longmsi147@gmail.com>
2025-07-04 20:50:45 +0700

    hehe

--- build/git_update.sh
+++ build/git_update.sh
@@ -11,12 +11,12 @@ do
 done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
-git fetch --tags --prune --unshallow 2>/dev/null
+git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v1.0.0'
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -48,16 +48,16 @@ echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=$(git tag --points-at $GIT_COMMIT)
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
   echo "Tagged with $NEW_TAG"
   git tag $NEW_TAG
-  git push origin $NEW_TAG
+  git push --tags
+  git push
 else
   echo "Already a tag on this commit"
-  NEW_TAG=$NEEDS_TAG
 fi
 
 echo "git-tag=$NEW_TAG" >> $GITHUB_OUTPUT
